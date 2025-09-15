a617049ee33e1f1b03f8530fdcfae48e70412a34
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-09-15 20:13:28 +0500

    working on home page

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>HomeBottomText</div>
+    <div className='font-[font2] flex items-center justify-center gap-6'>
+<Link className='text-[5vw] hover:border-[#D3FD50] hover:text-[#D3FD50] h-44px border-2 leading-[5vw] px-14 pt-2  uppercase border-white rounded-full  'to='projects'> Projects </Link>
+<Link className='text-[5vw] hover:border-[#D3FD50] hover:text-[#D3FD50] h-44px border-2 leading-[5vw] px-14 pt-2 uppercase border-white rounded-full  ' to='/agence'> Agence </Link>
+
+    </div>
   )
 }
 
