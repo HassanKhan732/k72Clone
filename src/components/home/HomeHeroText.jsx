0be820eaccaff4c5d7014999af611e2fc9f388af
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-09-15 18:56:56 +0500

    working

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Video from './Video'
 const HomeHeroText = () => {
   return (
-    <div>HomeHeroText</div>
+    <div className='font-[font2] pt-5 text-center'>
+      <div className='text-[9vw] justify-center uppercase leading-[8vw] flex items-center'>L'étincelle</div>
+      <div className='text-[9.5vw] justify-center uppercase leading-[8.5vw] flex items-start'>qui <div className='h-[7vw] w-[16vw] -mt-3 rounded-full overflow-hidden'><Video/></div> génère</div>
+      <div className='text-[9vw] justify-center uppercase leading-[8.5vw] flex items-center'>la créativité</div>
+    </div>
   )
 }
 
