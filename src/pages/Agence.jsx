05b25262b7c78fa15074e3d262b3b64e21c56e6a
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-09-19 18:29:49 +0500

    working on agence

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>Agence</div>
+    <div className='text-black font-[font2]'>
+      <div className=' mt-[55vh]'>
+        <h1 className='text-[19vw] uppercase text-center leading-[17vw]'>
+        Soixan7e
+        <br />Douze
+      </h1>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div className='h-screen w-screen fixed'>
     <Video/>
     </div>
-    <div className='h-screen w-screen relative flex flex-col justify-between'>
+    <div className='h-screen w-screen relative flex flex-col justify-between overflow-hidden'>
      <HomeHeroText/>
      <HomeBottomText/>
     </div>
