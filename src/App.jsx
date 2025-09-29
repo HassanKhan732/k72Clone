7d13311762d0927f603a4fdd21d11c0f8e896fe8
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-09-29 18:38:39 +0500

    gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,132 @@
-import React from 'react'
-import { Route , Routes } from 'react-router-dom'
-// import { Link } from 'react-router-dom'
+// import React from 'react'
+// import { Route , Routes } from 'react-router-dom'
+// // import { Link } from 'react-router-dom'
+// import Home from './pages/Home'
+// import Agence from './pages/Agence'
+// import Projects from './pages/Projects'
+// import { useGSAP } from '@gsap/react'
+// import gsap from "gsap";
+// import { useRef } from 'react'
+
+// const App = () => {
+//   const stairParentRef = useRef(null)
+//   useGSAP(function(){
+//     const tl = gsap.timeline()
+//       tl.to(stairParentRef.current,{
+//       display:'none'
+//     })
+//     tl.from('.stair',{
+//       height:0,
+//       stagger:{
+//         amount:-0.25,
+//       }
+//     })
+//     tl.to('.stair',{
+//       y:'100%',
+//       stagger:{
+//         amount:0.25
+//       }
+//     })
+//     tl.to(stairParentRef.current,{
+//       display:'none'
+//     })
+//        tl.to('.stair',{
+//       y:'0%',
+
+//     })
+//   })
+//   return (
+//     <div className='text-white'>
+
+// <div ref={stairParentRef} className="fixed z-20 h-screen w-full top-0"> 
+
+// <div className="h-full w-full flex"> 
+
+// <div className="stair h-full w-1/5 bg-black"></div>
+// <div className="stair h-full w-1/5 bg-black"></div>
+// <div className="stair h-full w-1/5 bg-black"></div>
+// <div className="stair h-full w-1/5 bg-black"></div>
+// <div className="stair h-full w-1/5 bg-black"></div>
+
+// </div>
+
+// </div>
+//       <Routes>
+//        <Route path='/' element = {<Home/>}/>
+//        <Route path='/agence' element = {<Agence/>}/>
+//        <Route path='/projects' element = {<Projects/>}/>
+
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import React, { useRef } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import { useGSAP } from '@gsap/react'
+import gsap from "gsap";
+
 const App = () => {
+  const stairParentRef = useRef(null)
+
+  useGSAP(() => {
+    const tl = gsap.timeline()
+
+    // Step 1: Ensure parent visible before animation
+    tl.set(stairParentRef.current, { display: "block" })
+
+    // Step 2: Animate stairs height from 0
+    tl.from(".stair", {
+      height: 0,
+      stagger: {
+        amount: 0.25,
+      },
+      duration: 0.5,
+      ease: "power2.out"
+    })
+
+    // Step 3: Move stairs down
+    tl.to(".stair", {
+      y: "100%",
+      stagger: {
+        amount: 0.25,
+      },
+      duration: 0.6,
+      ease: "power2.inOut"
+    })
+
+    // Step 4: Hide parent after animation completes
+    tl.set(stairParentRef.current, { display: "none" })
+  })
+
   return (
     <div className='text-white'>
-{
-/* <Link className = 'text-blue-500 mr-5' to = {'/'}>Home</Link>
-<Link className = 'text-blue-500 mr-5' to = {'/agence'}>Agence</Link>
-<Link className = 'text-blue-500 mr-5' to = {'/projects'}>Projects</Link> */
-}
+      {/* Overlay Animation */}
+      <div
+        ref={stairParentRef}
+        className="fixed z-20 h-screen w-full top-0 left-0"
+      >
+        <div className="h-full w-full flex">
+          <div className="stair h-full w-1/5 bg-black"></div>
+          <div className="stair h-full w-1/5 bg-black"></div>
+          <div className="stair h-full w-1/5 bg-black"></div>
+          <div className="stair h-full w-1/5 bg-black"></div>
+          <div className="stair h-full w-1/5 bg-black"></div>
+        </div>
+      </div>
 
+      {/* Routes */}
       <Routes>
-       <Route path='/' element = {<Home/>}/>
-       <Route path='/agence' element = {<Agence/>}/>
-       <Route path='/projects' element = {<Projects/>}/>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/agence" element={<Agence />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
   )
