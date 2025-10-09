27ac1df6b28339c8a2d4da62c248e3f5ec9b3869
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-10-09 17:14:21 +0500

    working on nav

---
+++ src/components/Navigation/FullScreenNav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div>
+        <div>
+
+            <div>
+                <h1>PROJECTS</h1>
+                <div>POUR TOUT VOIR</div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
