3200a320d21cb62758dc82adb1770063d7c3f577
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-10-09 17:46:47 +0500

    working on full screen nav

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -7,7 +7,13 @@ const FullScreenNav = () => {
 
             <div>
                 <h1>PROJECTS</h1>
-                <div>POUR TOUT VOIR</div>
+                <div>
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                </div>
+
             </div>
 
         </div>
