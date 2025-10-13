569113d1fa2ff9ef8222c08cb7d94f7afd529f22
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-10-13 19:41:35 +0500

    working

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -10,7 +10,59 @@ const FullScreenNav = () => {
           <div className='absolute flex top-0 bg-[#D3FD50] '>
 
    
-                    <div className='flex items-center flex-nowrap overflow-auto'>
+                    <div className='flex items-center flex-nowrap '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase text-black leading-[0.8] pt-4 text-center'>POUR TOUT VOIR</h2>
+                    <img className='w-64 h-28  rounded-full shrink-0 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase text-black leading-[0.8] pt-4 text-center'>POUR TOUT VOIR</h2>
+                    <img className='w-64 h-28  rounded-full shrink-0 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    </div>
+
+                    {/* <div>
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    </div> */}
+
+                    {/* <div>
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    </div> */}
+
+
+             </div>
+              <div className='absolute flex top-0 bg-[#D3FD50] '>
+
+   
+                    <div className='flex items-center flex-nowrap '>
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase text-black leading-[0.8] pt-4 text-center'>POUR TOUT VOIR</h2>
+                    <img className='w-64 h-28  rounded-full shrink-0 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase text-black leading-[0.8] pt-4 text-center'>POUR TOUT VOIR</h2>
+                    <img className='w-64 h-28  rounded-full shrink-0 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    </div>
+
+                    {/* <div>
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    </div> */}
+
+                    {/* <div>
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    <h2>POUR TOUT VOIR</h2>
+                    <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+                    </div> */}
+
+
+             </div>
+              <div className='absolute flex top-0 bg-[#D3FD50] '>
+
+   
+                    <div className='flex items-center flex-nowrap '>
                     <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase text-black leading-[0.8] pt-4 text-center'>POUR TOUT VOIR</h2>
                     <img className='w-64 h-28  rounded-full shrink-0 object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
                     <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase text-black leading-[0.8] pt-4 text-center'>POUR TOUT VOIR</h2>
